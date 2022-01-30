c4d9ada7830e8e5721b564a14f7d1dc80067751a
Mohammad Noor AbuKhleif <moh.noor94@gmail.com>
2022-01-30 15:51:41 +0200

    First Simple Controller

---
+++ src/main/java/guru/noor/controller/DemoController.java
@@ -0,0 +1,15 @@
+package guru.noor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
